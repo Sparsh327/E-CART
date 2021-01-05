54319d2e03d0c75ed251c33ba0866add83e0c148
Sparsh <sparsh.jas07@gmail.com>
2021-01-05 17:58:15 +0530

    Track order

---
+++ app/http/controllers/customer/orderController.js
@@ -0,0 +1,45 @@
+const Order = require("../../../models/order")
+const moment = require('moment')
+function orderController(){
+       return{
+           store(req,res){
+               //validate request
+
+               const {phone,address}=req.body
+               if(!phone||!address){
+                   req.flash('error','All fields are required')
+                   return res.render('/cart')
+               }
+               const order = new Order({
+                   customerId:req.user.id,
+                   items:req.session.cart.items,
+                   phone,
+                   address
+               })
+               order.save().then(result=>{
+                    req.flash('success','Order Placed Successfully')
+                    delete req.session.cart
+                    return res.redirect('/customer/orders')
+               }).catch(err=>{
+                     req.flash('error','Something went wrong')
+                     return res.redirect('/cart')
+               })
+           },
+           async index(req,res){
+               const orders = await Order.find({customerId:req.user._id},null, { sort: { 'createdAt': -1 }})
+               res.header('Cache-Control', 'no-cache, private, no-store, must-revalidate, max-stale=0, post-check=0, pre-check=0')
+               res.render('customers/orders',{orders:orders,moment:moment})
+
+           },
+           async show(req,res){
+               const order = await Order.findById(req.params.id)
+               //authorise
+               if(req.user._id.toString() === order.customerId.toString()){
+                   res.render('customers/singleOrder',{order})
+               }else{
+                   res.redirect('/')
+               }
+           }
+       }
+}
+module.exports = orderController
